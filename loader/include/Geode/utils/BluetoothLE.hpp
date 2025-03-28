d46a49e89a71290726760861b394a20a1a58780e
senya <76913432+senya5544@users.noreply.github.com>
2025-03-28 22:15:10 +0300

    goofy

--- loader/include/Geode/utils/BluetoothLE.hpp
+++ loader/include/Geode/utils/BluetoothLE.hpp
@@ -4,9 +4,9 @@
 #include <jni.h>
 
 namespace geode::utils::BluetoothLE {
-    void GEODE_DLL setOnScanResultCallback(std::function<void(jobject)> callback)
-    void GEODE_DLL setOnConnectionStateChangeCallback(std::function<void(jobject, jint, jint)> callback)
-    void GEODE_DLL setOnServicesDiscoveredCallback(std::function<void(jobject, jint)> callback)
-    void GEODE_DLL setOnCharacteristicReadCallback(std::function<void(jobject, jobject, jbyteArray, jint)> callback)
-    void GEODE_DLL setOnCharacteristicChangedCallback(std::function<void(jobject, jobject, jbyteArray)> callback)
+    void GEODE_DLL setOnScanResultCallback(std::function<void(jobject)> callback);
+    void GEODE_DLL setOnConnectionStateChangeCallback(std::function<void(jobject, jint, jint)> callback);
+    void GEODE_DLL setOnServicesDiscoveredCallback(std::function<void(jobject, jint)> callback);
+    void GEODE_DLL setOnCharacteristicReadCallback(std::function<void(jobject, jobject, jbyteArray, jint)> callback);
+    void GEODE_DLL setOnCharacteristicChangedCallback(std::function<void(jobject, jobject, jbyteArray)> callback);
 }
